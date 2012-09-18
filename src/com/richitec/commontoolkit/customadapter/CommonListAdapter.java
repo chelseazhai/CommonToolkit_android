8aa3caa02f31d40d74b38d801e7bed512b38fd9f
chelsea zhai <chelsea.zhai@gmail.com>
2012-09-19 06:13:06 +0800

    add view holder for commonListAdapter

--- src/com/richitec/commontoolkit/customadapter/CommonListAdapter.java
+++ src/com/richitec/commontoolkit/customadapter/CommonListAdapter.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import android.content.Context;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,19 +53,36 @@ public abstract class CommonListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		// define view holer object
+		ViewHolder _viewHolder;
+
+		// check convert view
 		if (null == convertView) {
 			convertView = _mLayoutInflater.inflate(_mItemsLayoutResId, null);
+
+			// init view holder and set its views for holding
+			_viewHolder = new ViewHolder();
+			// set item component view subViews
+			for (int i = 0; i < _mItemsComponentResIds.length; i++) {
+				_viewHolder.views4Holding.append(_mItemsComponentResIds[i],
+						convertView.findViewById(_mItemsComponentResIds[i]));
+			}
+
+			// set tag
+			convertView.setTag(_viewHolder);
+		} else {
+			// get view holder
+			_viewHolder = (ViewHolder) convertView.getTag();
 		}
 
 		// set item component view subViews
 		for (int i = 0; i < _mItemsComponentResIds.length; i++) {
 			// bind item component view data
-			bindView(convertView.findViewById(_mItemsComponentResIds[i]),
+			// bindView(convertView.findViewById(_mItemsComponentResIds[i]),
+			// _mData.get(position), _mDataKeys[i]);
+			bindView(_viewHolder.views4Holding.get(_mItemsComponentResIds[i]),
 					_mData.get(position), _mDataKeys[i]);
 		}
-
-		// set tag
-		convertView.setTag(position);
 
 		return convertView;
 	}
@@ -72,5 +90,19 @@ public abstract class CommonListAdapter extends BaseAdapter {
 	// bind view and data
 	protected abstract void bindView(View view, Map<String, ?> dataMap,
 			String dataKey);
+
+	// inner class
+	class ViewHolder {
+		// views for holding
+		SparseArray<View> views4Holding;
+
+		public ViewHolder() {
+			super();
+
+			// init views sparse array for holding
+			views4Holding = new SparseArray<View>();
+		}
+
+	}
 
 }
