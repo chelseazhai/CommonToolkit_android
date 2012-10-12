a25761dbd9016071c30cd73f39846a37be42d17a
chelsea zhai <chelsea.zhai@gmail.com>
2012-10-12 17:20:04 +0800

    add commonUtils to convert array to list

---
+++ src/com/richitec/commontoolkit/utils/CommonUtils.java
@@ -0,0 +1,21 @@
+package com.richitec.commontoolkit.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonUtils {
+
+	// convert array to list
+	public static List<Object> array2List(Object[] array) {
+		List<Object> _ret = new ArrayList<Object>();
+
+		if (null != array) {
+			for (int i = 0; i < array.length; i++) {
+				_ret.add(array[i]);
+			}
+		}
+
+		return _ret;
+	}
+
+}
