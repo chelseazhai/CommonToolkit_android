44f7384efcca9d88d9a67f6369c60822b0878ecd
chelsea zhai <chelsea.zhai@gmail.com>
2013-01-06 17:09:44 +0800

    update common utils

--- src/com/richitec/commontoolkit/utils/CommonUtils.java
+++ src/com/richitec/commontoolkit/utils/CommonUtils.java
@@ -3,13 +3,17 @@ package com.richitec.commontoolkit.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.util.Log;
 
 import com.richitec.commontoolkit.activityextension.AppLaunchActivity;
 
 public class CommonUtils {
+
+	private static final String LOG_TAG = "CommonUtils";
 
 	// convert array to list
 	public static List<?> array2List(Object[] array) {
@@ -26,12 +30,28 @@ public class CommonUtils {
 
 	// check if intent available or not
 	public static boolean isIntentAvailable(Intent intent) {
+		// define return result
+		boolean _ret = true;
+
+		// get application context
+		Context _appContext = AppLaunchActivity.getAppContext();
+
 		// get and check intent resolve info list
-		List<ResolveInfo> _resolveInfoList = AppLaunchActivity.getAppContext()
-				.getPackageManager()
+		List<ResolveInfo> _resolveInfoList = _appContext.getPackageManager()
 				.queryIntentActivities(intent, PackageManager.GET_ACTIVITIES);
 
-		return _resolveInfoList.size() > 0;
+		// check
+		if (_resolveInfoList.size() <= 0) {
+			// update return result
+			_ret = false;
+
+			Log.e(LOG_TAG, "Intent = " + intent + " is not available");
+
+			// // show intent is not available message
+			// Toast.makeText(_appContext, "??", Toast.LENGTH_LONG).show();
+		}
+
+		return _ret;
 	}
 
 }
