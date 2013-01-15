a754067e6d1f8b0d6dd494560ff4f0e46c696f6e
chelsea zhai <chelsea.zhai@gmail.com>
2013-01-15 19:16:29 +0800

    tmp saved

--- src/com/richitec/commontoolkit/activityextension/AppInstructionActivity.java
+++ src/com/richitec/commontoolkit/activityextension/AppInstructionActivity.java
@@ -1,16 +1,50 @@
 package com.richitec.commontoolkit.activityextension;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.richitec.commontoolkit.R;
 
 public class AppInstructionActivity extends Activity {
 
+	private static final String LOG_TAG = "AppInstructionActivity";
+
+	// key for need to launch application instruction
+	public static final String APPINSTRUCTION_LAUNCHFLAG = "need_to_launch_application_instruction";
+
+	// application instruction activity onCreate param key
+	public static final String APPINSTRUCTION_ACTIVITY_TARGETINTENT_PARAM_KEY = "application_target_intent";
+	public static final String APPINSTRUCTION_ACTIVITY_IMGRESIDS_PARAM_KEY = "application_instruction_content_image_resource_id_list";
+
+	// application target intent
+	private Intent _mAppTargetIntent;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// set content view
 		setContentView(R.layout.app_instruction_activity_layout);
+
+		// get the intent parameter data
+		Bundle _data = getIntent().getExtras();
+
+		// check the data bundle
+		if (null != _data
+				&& null != _data
+						.getIntegerArrayList(APPINSTRUCTION_ACTIVITY_IMGRESIDS_PARAM_KEY)
+				&& null != _data
+						.getParcelable(APPINSTRUCTION_ACTIVITY_TARGETINTENT_PARAM_KEY)) {
+			// init application instruction content image list
+			//
+
+			// save application target intent
+			_mAppTargetIntent = _data
+					.getParcelable(APPINSTRUCTION_ACTIVITY_TARGETINTENT_PARAM_KEY);
+		}
 	}
+
+	//
 
 }
--- src/com/richitec/commontoolkit/activityextension/AppLaunchActivity.java
+++ src/com/richitec/commontoolkit/activityextension/AppLaunchActivity.java
@@ -1,5 +1,8 @@
 package com.richitec.commontoolkit.activityextension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -9,6 +12,7 @@ import android.util.Log;
 import android.widget.ImageView;
 
 import com.richitec.commontoolkit.R;
+import com.richitec.commontoolkit.utils.DataStorageUtils;
 
 public abstract class AppLaunchActivity extends Activity {
 
@@ -25,12 +29,17 @@ public abstract class AppLaunchActivity extends Activity {
 		((ImageView) findViewById(R.id.appSplash_imageView))
 				.setImageDrawable(splashImg());
 
-		// new application launching task to do didFinishLaunching in background
-		new AppLaunchingTask().execute(intentActivity());
+		// new application launching instruction and task to do
+		// didFinishLaunching in background
+		new AppLaunchingTask().execute(instructionContentImgResIds(),
+				intentActivity());
 	}
 
 	// application loading splash image
 	public abstract Drawable splashImg();
+
+	// application instruction content images resource id
+	public abstract List<Integer> instructionContentImgResIds();
 
 	// application intent activity
 	public abstract Intent intentActivity();
@@ -40,19 +49,27 @@ public abstract class AppLaunchActivity extends Activity {
 
 	// inner class
 	// application launching task
-	class AppLaunchingTask extends AsyncTask<Intent, Integer, Integer> {
+	class AppLaunchingTask extends AsyncTask<Object, Integer, Integer> {
 
-		// task intent
+		// instruction image resource id list
+		List<Integer> _mInstructionImgResIds;
+
+		// target intent
 		Intent _mIntent;
 
 		@Override
-		protected Integer doInBackground(Intent... params) {
+		protected Integer doInBackground(Object... params) {
 			// init return result
 			Integer _ret = -1;
 
-			// save task intent
-			if (1 == params.length) {
-				_mIntent = params[params.length - 1];
+			// save instruction image list and target intent
+			if (2 == params.length) {
+				// workaround by ares
+				@SuppressWarnings("unchecked")
+				List<Integer> _instructionImgResIds = (List<Integer>) params[0];
+
+				_mInstructionImgResIds = _instructionImgResIds;
+				_mIntent = (Intent) params[params.length - 1];
 
 				// did finish launch
 				didFinishLaunching();
@@ -76,8 +93,46 @@ public abstract class AppLaunchActivity extends Activity {
 			if (0 == result) {
 				// check intent activity
 				if (null != _mIntent) {
-					// go to intent activity
-					startActivity(_mIntent);
+					// get application instruction launch flag
+					Boolean _need2LaunchAppInstruction = DataStorageUtils
+							.getBoolean(AppInstructionActivity.APPINSTRUCTION_LAUNCHFLAG);
+
+					// need to launch application instruction
+					if (null == _need2LaunchAppInstruction
+							|| true == _need2LaunchAppInstruction) {
+						// check application instruction content image list
+						if (null != _mInstructionImgResIds
+								&& !_mInstructionImgResIds.isEmpty()) {
+							// define application instruction intent
+							Intent _appInstructionIntent = new Intent(
+									AppLaunchActivity.this,
+									AppInstructionActivity.class);
+
+							// put instruction content image list and intent
+							// activity
+							_appInstructionIntent
+									.putIntegerArrayListExtra(
+											AppInstructionActivity.APPINSTRUCTION_ACTIVITY_IMGRESIDS_PARAM_KEY,
+											(ArrayList<Integer>) _mInstructionImgResIds);
+							_appInstructionIntent
+									.putExtra(
+											AppInstructionActivity.APPINSTRUCTION_ACTIVITY_TARGETINTENT_PARAM_KEY,
+											_mIntent);
+
+							// go to application instruction activity
+							startActivity(_appInstructionIntent);
+						} else {
+							Log.w(LOG_TAG,
+									"launch application instruction error, instruction image = "
+											+ _mInstructionImgResIds);
+
+							// go to intent activity
+							startActivity(_mIntent);
+						}
+					} else {
+						// go to intent activity
+						startActivity(_mIntent);
+					}
 				} else {
 					Log.e(LOG_TAG, "intent activity is null");
 				}
