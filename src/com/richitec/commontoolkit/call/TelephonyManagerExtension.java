62f3f410509fcbaaacfb0ebd1d25e6b48bf504e1
chelsea zhai <chelsea.zhai@gmail.com>
2013-02-02 13:42:27 +0800

    add answer the currently-ringing incoming call method to telephonyManagerextension

--- src/com/richitec/commontoolkit/call/TelephonyManagerExtension.java
+++ src/com/richitec/commontoolkit/call/TelephonyManagerExtension.java
@@ -33,19 +33,11 @@ public class TelephonyManagerExtension {
 		return _telephonyManager;
 	}
 
-	// reject incoming call
+	// reject the currently-ringing incoming call
 	public static void rejectIncomingCall() {
 		try {
-			// get get telephony interface method
-			Method _getITelephonyMethod = TelephonyManager.class
-					.getDeclaredMethod("getITelephony", (Class[]) null);
-
-			// set get telephony interface method access permission
-			_getITelephonyMethod.setAccessible(true);
-
 			// get telephony interface object
-			Object _ITelephonyObject = _getITelephonyMethod.invoke(
-					getTelephonyManager(), (Object[]) null);
+			Object _ITelephonyObject = getITelephony();
 
 			// get end call method
 			Method _endCallMethod = _ITelephonyObject.getClass()
@@ -56,25 +48,121 @@ public class TelephonyManagerExtension {
 
 			// reject the incoming call
 			_endCallMethod.invoke(_ITelephonyObject, (Object[]) null);
+		} catch (ClassNotFoundException e) {
+			Log.e(LOG_TAG,
+					"Cann't find telephony interface class, exception message = "
+							+ e.getMessage());
+
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			Log.e(LOG_TAG,
+					"Handle telephony interface with security error, exception message = "
+							+ e.getMessage());
+
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			Log.e(LOG_TAG,
+					"Get method of 'endCall' error, exception message = "
+							+ e.getMessage());
+
+			e.printStackTrace();
+		} catch (Exception e) {
+			Log.e(LOG_TAG,
+					"Invoke method of 'endCall' error, exception message = "
+							+ e.getMessage());
+
+			e.printStackTrace();
+		}
+	}
+
+	// answer the currently-ringing incoming call
+	public static void answerIncomingCall() {
+		try {
+			// get telephony interface object
+			Object _ITelephonyObject = getITelephony();
+
+			// get answer ringing call method
+			Method _answerRingingCallMethod = _ITelephonyObject.getClass()
+					.getDeclaredMethod("answerRingingCall", (Class[]) null);
+
+			// set answer ringing call method access permission
+			_answerRingingCallMethod.setAccessible(true);
+
+			// answer the currently-ringing incoming call
+			_answerRingingCallMethod.invoke(_ITelephonyObject, (Object[]) null);
+		} catch (ClassNotFoundException e) {
+			Log.e(LOG_TAG,
+					"Cann't find telephony interface class, exception message = "
+							+ e.getMessage());
+
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			Log.e(LOG_TAG,
+					"Handle telephony interface with security error, exception message = "
+							+ e.getMessage());
+
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			Log.e(LOG_TAG,
+					"Get method of 'answerRingingCall' error, exception message = "
+							+ e.getMessage());
+
+			e.printStackTrace();
+		} catch (Exception e) {
+			Log.e(LOG_TAG,
+					"Invoke method of 'answerRingingCall' error, exception message = "
+							+ e.getMessage());
+
+			e.printStackTrace();
+		}
+	}
+
+	// get telephony interface object
+	private static Object getITelephony() throws ClassNotFoundException {
+		// define telephony interface object
+		Object _ITelephonyObject = null;
+
+		try {
+			// get get telephony interface method
+			Method _getITelephonyMethod = TelephonyManager.class
+					.getDeclaredMethod("getITelephony", (Class[]) null);
+
+			// set get telephony interface method access permission
+			_getITelephonyMethod.setAccessible(true);
+
+			// get telephony interface object
+			_ITelephonyObject = _getITelephonyMethod.invoke(
+					getTelephonyManager(), (Object[]) null);
 		} catch (SecurityException e) {
 			Log.e(LOG_TAG,
 					"Handle telephony manager with security error, exception message = "
 							+ e.getMessage());
 
 			e.printStackTrace();
+
+			// throw ITelephony class not found exception
+			throw new ClassNotFoundException("ITelephony class not found");
 		} catch (NoSuchMethodException e) {
 			Log.e(LOG_TAG,
-					"Get method of 'getITelephony' or 'endCall' error, exception message = "
+					"Get method of 'getITelephony' error, exception message = "
 							+ e.getMessage());
 
 			e.printStackTrace();
+
+			// throw ITelephony class not found exception
+			throw new ClassNotFoundException("ITelephony class not found");
 		} catch (Exception e) {
 			Log.e(LOG_TAG,
-					"Invoke method of 'getITelephony' or 'endCall' error, exception message = "
+					"Invoke method of 'getITelephony' error, exception message = "
 							+ e.getMessage());
 
 			e.printStackTrace();
+
+			// throw ITelephony class not found exception
+			throw new ClassNotFoundException("ITelephony class not found");
 		}
+
+		return _ITelephonyObject;
 	}
 
 }
